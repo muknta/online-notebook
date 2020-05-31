b6f8012b16d311c9f4ddf4182d86d6a63e6ea323
heknt <hekntsnv@gmail.com>
2020-05-31 04:02:52 +0300

    updated Procfile

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ from credentials import (
 class Config:
 	DEBUG = True
 	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(16)
-	SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}/{}?reconnect=true'.format(
+	SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}/{}'.format(
 					DB_DIALECT, DB_LOGIN, DB_PASSWORD, DB_HOST, DB_NAME)
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -5,8 +5,7 @@ myapp = app
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	myapp.run(port=port)
-
+	myapp.run()
 
 
 from notes.models import Note, User
